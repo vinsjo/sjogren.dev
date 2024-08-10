924dcb121944382d2aae032713a1d4adb55b7887
Vincent Sjögren <vincent.sjogren@bitlog.se>
2024-08-10 17:09:13 +0200

    did some cleaning up to github-api

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -3,3 +3,5 @@ export enum PageSection {
   Projects = 'projects',
   Contact = 'contact',
 }
+
+export const IS_DEV = process.env.NODE_ENV === 'development';
--- src/utils/api/github-api.ts
+++ src/utils/api/github-api.ts
@@ -3,6 +3,7 @@ import path from 'path';
 import axios from 'axios';
 import { Octokit } from '@octokit/rest';
 import { pick } from '../misc';
+import { IS_DEV } from '@/constants';
 
 export type Repo = Awaited<
   ReturnType<Octokit['rest']['repos']['listForAuthenticatedUser']>
@@ -32,23 +33,23 @@ export type PartialRepo = Pick<Repo, PartialRepoKey> & {
 
 type PackageJSON = Readonly<{
   name: string;
-  version: string;
-  description: string;
-  author: string;
-  license: string;
-  repository: Record<string, string>;
-  type: string;
-  source: string;
-  main: string;
-  module: string;
-  types: string;
-  exports: string;
-  files: string[];
-  browserslist: string[];
-  scripts: Record<string, string>;
-  devDependencies: Record<string, string>;
-  dependencies: Record<string, string>;
-  keywords: string[];
+  // version: string;
+  // description: string;
+  // author: string;
+  // license: string;
+  // repository: Record<string, string>;
+  // type: string;
+  // source: string;
+  // main: string;
+  // module: string;
+  // types: string;
+  // exports: string;
+  // files: string[];
+  // browserslist: string[];
+  // scripts: Record<string, string>;
+  // devDependencies: Record<string, string>;
+  // dependencies: Record<string, string>;
+  // keywords: string[];
 }>;
 
 const PROFILE_REPO_NAME = 'vinsjo';
@@ -89,7 +90,10 @@ async function fetchAllRepos(): Promise<Repo[]> {
   return response.data;
 }
 
-function getPackageURL({ full_name, default_branch }: Repo): string | null {
+function getPackageURL({
+  full_name,
+  default_branch,
+}: Pick<Repo, 'full_name' | 'default_branch'>): string | null {
   return !full_name || !default_branch
     ? null
     : `https://raw.githubusercontent.com/${full_name}/${default_branch}/package.json`;
@@ -97,16 +101,13 @@ function getPackageURL({ full_name, default_branch }: Repo): string | null {
 
 function isPackageJsonLanguage(language: Maybe<string>): boolean {
   return (
-    language != null &&
-    ['TypeScript', 'JavaScript'].some(
-      (lang) =>
-        lang === language || lang.toLowerCase() === language.toLowerCase(),
-    )
+    typeof language === 'string' &&
+    ['typescript', 'javascript'].includes(language.toLowerCase())
   );
 }
 
 async function fetchPackageJSON(
-  repo: Repo,
+  repo: Pick<Repo, 'full_name' | 'default_branch' | 'language'>,
 ): Promise<Partial<PackageJSON> | null> {
   const NO_URL_ERROR = 'NO_URL';
   const INVALID_LANGUAGE_ERROR = 'INVALID_LANGUAGE';
@@ -129,64 +130,62 @@ async function fetchPackageJSON(
     });
     return data;
   } catch (err) {
-    if (
-      process.env.NODE_ENV !== 'production' &&
-      err !== NO_URL_ERROR &&
-      err !== INVALID_LANGUAGE_ERROR &&
-      (!axios.isAxiosError(err) || err.response?.status !== 404)
-    ) {
-      console.error(err instanceof Error ? err.message : err);
+    if (IS_DEV) {
+      if (
+        err !== NO_URL_ERROR &&
+        err !== INVALID_LANGUAGE_ERROR &&
+        (!axios.isAxiosError(err) || err.response?.status !== 404)
+      ) {
+        console.error(err instanceof Error ? err.message : err);
+      }
     }
+
     return null;
   }
 }
 
-async function getPackageJsonName(repo: Repo): Promise<string | null> {
+async function getPackageJsonName(
+  repo: Pick<Repo, 'full_name' | 'default_branch' | 'language'>,
+): Promise<string | null> {
   const pkg = await fetchPackageJSON(repo);
 
-  return (pkg && pkg.name) || null;
-}
-
-function filterRepos(repos: Repo[]) {
-  return repos.filter(
-    (repo): boolean =>
-      // Exclude forked repos
-      !repo.fork &&
-      // Exclude "profile repo"
-      repo.name !== PROFILE_REPO_NAME &&
-      // Exclude school assignments
-      !repo.topics?.includes('school-assignment') &&
-      // Exclude repos missing description
-      !!repo.description &&
-      // Only include repos with at least three words in description
-      /([\w.-@/!?&%]+(\s)+){2,}[\w.-@/!?&%]+/.test(repo.description),
-  );
-}
-
-async function createPartialRepo(repo: Repo): Promise<PartialRepo> {
-  const partialRepo: PartialRepo = {
-    ...pick(repo, ...repoKeys),
-    package_name: await getPackageJsonName(repo),
-  };
-
-  if (partialRepo.name === THIS_REPO_NAME) {
-    partialRepo.description = 'This website';
-    partialRepo.homepage = partialRepo.html_url;
-  }
-
-  return partialRepo;
-}
-
-function sortRepos<T extends Partial<Repo>>(repos: T[]) {
-  return repos.sort((a, b) =>
-    a.name === THIS_REPO_NAME ? 1 : b.name === THIS_REPO_NAME ? -1 : 0,
-  );
+  return pkg?.name || null;
 }
 
 export async function fetchRepos(): Promise<PartialRepo[]> {
   const repos = await fetchAllRepos();
 
+  const filteredRepos = repos
+    .filter(
+      (repo): boolean =>
+        // Exclude forked repos
+        !repo.fork &&
+        // Exclude "profile repo"
+        repo.name !== PROFILE_REPO_NAME &&
+        // Exclude school assignments
+        !repo.topics?.includes('school-assignment') &&
+        // Exclude repos missing description
+        !!repo.description?.trim() &&
+        // Only include repos with at least three words in description
+        /([\w.-@/!?&%]+(\s)+){2,}[\w.-@/!?&%]+/.test(repo.description),
+    )
+    .sort((a, b) =>
+      a.name === THIS_REPO_NAME ? 1 : b.name === THIS_REPO_NAME ? -1 : 0,
+    );
+
   return await Promise.all(
-    sortRepos(filterRepos(repos)).map(createPartialRepo),
+    filteredRepos.map(async (repo): Promise<PartialRepo> => {
+      const partialRepo: PartialRepo = {
+        ...pick(repo, ...repoKeys),
+        package_name: await getPackageJsonName(repo),
+      };
+
+      if (partialRepo.name === THIS_REPO_NAME) {
+        partialRepo.description = 'This website';
+        partialRepo.homepage = partialRepo.html_url;
+      }
+
+      return partialRepo;
+    }),
   );
 }
