494d8c81feb5731d8a0b39a1c29a6937dd2f6aa4
Vincent Sjögren <vincent.sjogren@bitlog.se>
2025-07-12 20:03:59 +0200

    added astro-compress and hashing of file names

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,20 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 
+import compress from 'astro-compress';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()],
+  integrations: [react(), compress()],
+  vite: {
+    build: {
+      rollupOptions: {
+        output: {
+          chunkFileNames: 'js/[hash].js',
+          entryFileNames: 'js/[hash].js',
+          assetFileNames: 'assets/[hash][extname]',
+        },
+      },
+    },
+  },
 });
